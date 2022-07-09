d5ede0fb05177e8e8b5d64468a10deaa3dfd85df
J-Josu <josue.sa.trabajos@gmail.com>
2022-07-09 02:20:36 -0300

    Fixed gh-pages conflict and project reorder

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,19 +8,31 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-      pages: "docs",
-      assets: "docs"
-    }),
-    paths: {
-			base: dev ? '' : '/Quiz-FOD',
-		},
-    appDir: 'internal',
-    prerender : {
-      default: true
-    },
-    trailingSlash: 'always'
+		adapter: adapter()
 	}
 };
+
+// import adapter from '@sveltejs/adapter-static';
+// import preprocess from 'svelte-preprocess';
+// 
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   preprocess: preprocess(),
+
+//   kit: {
+//     adapter: adapter({
+//       pages: "docs",
+//       assets: "docs"
+//     }),
+//     paths: {
+//       base: dev ? 'localhost:3000' : '/Quiz-FOD',
+//     },
+//     appDir: 'internal',
+//     prerender : {
+//       default: true
+//     },
+//     trailingSlash: 'always'
+//   }
+// };
 
 export default config;
